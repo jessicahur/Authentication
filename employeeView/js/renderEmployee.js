0cf957c3a59c5478d13fa4ed0e8c4f987455b192
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-29 14:09:07 -0800

    minor change in js

--- employeeView/js/renderEmployee.js
+++ employeeView/js/renderEmployee.js
@@ -1,5 +1,6 @@
 var employeeApp = angular.module( 'employeeApp', []);
 
+//no ES6 arrow function bc it seems to fail in Safari
 employeeApp.controller('EmployeeController', function($scope, $http) {
 
   $http.get('/employees').then( function( res ) {
